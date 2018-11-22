22bd8722ce9d0ba1886f5be24e6ad0f88059c18e
Hermann Hueck <hermann.hueck@mailbox.org>
2018-11-22 16:23:00 +0100

    switched to akka-stream 2.5.18, scalatest 3.0.5, scalacheck 1.14.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,8 +1,8 @@
 import sbt._
 
 object Dependencies {
-  lazy val akkaStreams = "com.typesafe.akka" %% "akka-stream" % "2.5.12"
+  lazy val akkaStreams = "com.typesafe.akka" %% "akka-stream" % "2.5.18"
   lazy val spark = "org.apache.spark" %% "spark-core" % "2.4.0"
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.4"
-  lazy val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.13.5"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
+  lazy val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.14.0"
 }
